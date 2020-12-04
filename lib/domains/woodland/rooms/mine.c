e51e302110e4cc0659687741465e3b942d5b2176
evilmog <evilmog@ibm.com>
2020-12-04 13:51:31 -0700

    update woodland mines

--- lib/domains/woodland/rooms/mine.c
+++ lib/domains/woodland/rooms/mine.c
@@ -9,7 +9,10 @@ void setup( void ) {
   set_dimensions(({ 1, 1 }));
   add_area( "woodland" );
   set_short( "Woodland Crypto Currency Mines" );
-  set_long( "So years ago the company sold off all its mines, it replaced them with digital currency mines. All the corezite that was here was mined out years ago to make the fusion reactors that make up this digital mine. Chairs are all over the place lined up against terminals.\n\nYou should try to '%^EXT_129%^crypto%^RESET%^' here." );
+  set_long( "I know what you are thinking, why the heck is a crypto currency mine in a forest... To be honest there " +
+  	"is no real answer, if you have a problem with that take it up with management... Oh wait they don't care either. " +
+  	"Somehow there is a terminal embedded in a tree, to be honest I'm surprised it works, and yet here we are."
+  	"\n\nYou should try to '%^EXT_129%^crypto%^RESET%^' here." );
 
   add_action("do_crypto", "crypto");
 
