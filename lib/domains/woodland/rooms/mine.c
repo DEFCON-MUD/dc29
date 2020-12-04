6963aa66cea0a6b1e956cb56fad842ea4a8f7fe9
evilmog <evilmog@ibm.com>
2020-12-04 13:52:15 -0700

    typo fix

--- lib/domains/woodland/rooms/mine.c
+++ lib/domains/woodland/rooms/mine.c
@@ -11,7 +11,7 @@ void setup( void ) {
   set_short( "Woodland Crypto Currency Mines" );
   set_long( "I know what you are thinking, why the heck is a crypto currency mine in a forest... To be honest there " +
   	"is no real answer, if you have a problem with that take it up with management... Oh wait they don't care either. " +
-  	"Somehow there is a terminal embedded in a tree, to be honest I'm surprised it works, and yet here we are."
+  	"Somehow there is a terminal embedded in a tree, to be honest I'm surprised it works, and yet here we are." +
   	"\n\nYou should try to '%^EXT_129%^crypto%^RESET%^' here." );
 
   add_action("do_crypto", "crypto");
